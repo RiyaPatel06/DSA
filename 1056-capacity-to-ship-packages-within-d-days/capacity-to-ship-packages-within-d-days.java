eb02d41307ad86d9c033efd1cd0a09325b43ad37
RiyaPatel06 <riyu2020patel123@gmail.com>
2026-06-29 12:56:37 +0530

    Time: 10 ms (84.83%) | Memory: 50.5 MB (6.18%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,50 +1,32 @@
 class Solution {
-
-    private boolean canShip(int[] weights, int days, int capacity) {
-
-        int daysUsed = 1;
-        int currentLoad = 0;
-
-        for (int weight : weights) {
-
-            if (currentLoad + weight <= capacity) {
-                currentLoad += weight;
-            } else {
-                daysUsed++;
-                currentLoad = weight;
-
-                if (daysUsed > days)
-                    return false;
-            }
-        }
-
-        return true;
-    }
-
     public int shipWithinDays(int[] weights, int days) {
+        int l=0,h=0,max=0,ans=0;
 
-        int low = 0;
-        int high = 0;
-
-        for (int weight : weights) {
-            low = Math.max(low, weight);
-            high += weight;
+        for(int n:weights){
+            l=Math.max(l,n);
+            h+=n;
         }
 
-        int ans = high;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            int minDaysNeed=1;
+            int load=0;
 
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (canShip(weights, days, mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            for(int num:weights){
+                if(num+load>mid){
+                    load=num;
+                    minDaysNeed++;
+                }else{
+                    load+=num;
+                }
+            }
+            if(minDaysNeed<=days){
+                ans=mid;
+                h=mid-1;
+            }else{
+                l=mid+1;
             }
         }
-
         return ans;
     }
 }
