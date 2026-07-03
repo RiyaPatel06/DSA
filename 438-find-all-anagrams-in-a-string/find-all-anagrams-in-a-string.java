3f0dcd692d44d559a332064059908e5c63bf3fcd
RiyaPatel06 <riyu2020patel123@gmail.com>
2026-07-04 00:28:23 +0530

    Time: 13 ms (43.78%) | Memory: 46.6 MB (82.88%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,29 +1,33 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-        int n = s.length();
-        int m = p.length();
-        if(n < m) {
-            return res;
-        }
-        int []freq1 = new int[26];
-        int []freq2 = new int[26];
-        for(char ch : p.toCharArray()) {
-            freq1[ch-'a']++;
-        }
-        for(int i=0;i<m;i++) {
-            freq2[s.charAt(i)-'a']++;
-        }
-        if(Arrays.equals(freq1,freq2)) {
-            res.add(0);
-        }
-        for(int i=m;i<n;i++) {
-            freq2[s.charAt(i)-'a']++;
-            freq2[s.charAt(i-m)-'a']--;
-            if(Arrays.equals(freq1,freq2)) {
-                res.add(i-m+1);
-            }
-        }
-        return res;
+         
+
+    int[] pCount = new int[26];
+    int[] sCount = new int[26];
+    List<Integer> result = new java.util.ArrayList<>();
+
+    // Count frequency of characters in p
+    for (char c : p.toCharArray()) {
+      pCount[c - 'a']++;
     }
+
+    // Sliding window to count frequency of characters in s
+    for (int i = 0; i < s.length(); i++) {
+      sCount[s.charAt(i) - 'a']++;
+
+      // Remove the character that is out of the window
+      if (i >= p.length()) {
+        sCount[s.charAt(i - p.length()) - 'a']--;
+      }
+
+      // Compare counts
+      if (Arrays.equals(pCount, sCount)) {
+        result.add(i - p.length() + 1);
+      }
+    }
+
+    return result;
+  }
+
+    
 }
