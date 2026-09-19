449e074a58449c08e8ce4e54ad107baa6730259b
RiyaPatel06 <riyu2020patel123@gmail.com>
2026-09-19 12:22:24 +0530

    Time: 2 ms (99.94%) | Memory: 52.5 MB (65.03%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,21 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
        int n = nums.length;
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> mpp = new HashMap<>();
-        int mini = n / 3 + 1;
-
-        for (int i = 0; i < n; i++) {
-            mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
-
-            // Add to result only when the count just reaches mini
-            if (mpp.get(nums[i]) == mini) {
-                result.add(nums[i]);
-            }
-
-            if (result.size() == 2) break;
+       int cand1=Integer.MIN_VALUE;
+       int count1=0;
+       int cand2=Integer.MIN_VALUE;
+       int count2=0;
+        for(int i=0;i<n;i++){
+          if(count1==0 && cand2!=nums[i]){
+            count1=1;
+            cand1=nums[i];
+          }else if(count2==0 && cand1!=nums[i]){
+            count2=1;
+            cand2=nums[i];
+          }else if(cand1==nums[i]){
+            count1++;
+          }else if(cand2==nums[i]){
+            count2++;
+          }else{
+            count1--;
+            count2--;
+          }
+        }  
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==cand1) count1++;
+            if(nums[i]==cand2) count2++;
+        } 
+        int mini=n/3+1;
+        List<Integer> res=new ArrayList<>();
+        if(count1 >= mini) res.add(cand1);
+        if(count2 >= mini){
+            res.add(cand2);
         }
+        return res;
 
-        return result;
     }
 }
